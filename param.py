134041397c4ad7848fac18c694d22b57cd6e3b3c
Mark Garcia <mark@mgarcia.local>
2020-02-25 16:44:43 -0600

    Updated param.py

--- param.py
+++ param.py
@@ -1,13 +1,14 @@
 import json
 
+
 class Parameter:
 
-   def __init__(self,key, value):
-         self.key=key
-         self.value=value
-         return
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        return
 
-   def toJSON(self):
-       return json.dumps(self,default=lambda o: o.__dict__, sort_keys=True, indent=None)
-       # return json.dumps(self, default=lambda o: o.__dict__,
-       #                   sort_keys=True, indent=4)
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=None)
+        # return json.dumps(self, default=lambda o: o.__dict__,
+        #                   sort_keys=True, indent=4)
