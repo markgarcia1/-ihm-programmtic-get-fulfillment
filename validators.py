bd0833204b40667b23d4e01a184811a42be98d06
Mark Garcia <mark@mgarcia.local>
2020-02-25 16:40:22 -0600

    Updated validators.py to fix logic for POST data validation.

--- validators.py
+++ validators.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 
 from customExceptions import MissingParameterException, DateFormatException, PostDataException
 
@@ -17,23 +18,64 @@ def validate_get_parameters_not_null(event):
 
 def validate_post_data(event):
     try:
-        if event is None:
+        print("Validators: validating POST DATA: " + event["body"])
+        if isinstance(event, list):
+            print("Validators:: event is a List object.")
+        elif isinstance(event, dict):
+            print("Validators:  event is a dictionary.")
+        if event is None or "body" not in event.keys():
             pde = PostDataException()
             pde.message = "POST requires at least a single document."
-            print("Error: {}".format(pde))
+            print("Validators: Error: {}".format(pde))
             raise
-        docs = event
-        for doc in docs:
-            if doc["PageID"] is None or doc["PageID"] == "":
+        post_data = event["body"]
+        print("Validators: POST Data = " + json.dumps(post_data))
+        if isinstance(post_data, list):
+            print("Validators: POST data is a List.")
+            for data in post_data:
+                print("Validators:  Document = " + str(data))
+                doc = json.loads(data)
+                if "PageID" not in doc.keys() or doc["PageID"] == "":
+                    pde = PostDataException()
+                    pde.message = "Document PageID is Null or empty."
+                    print("Validators:  Error: {}".format(pde))
+                    raise
+        else:
+            if isinstance(post_data, dict):
+                print("Validators:  POST data is a dictionary.")
+            else:
+                print("Validators:  POST data is not a dictionary.")
+            doc = json.loads(post_data)
+            print("Validators: doc = {}".format(doc))
+            print("Validators: doc.keys() = {}".format(doc.keys()))
+            print("Validators: doc[PageID] = {}".format(doc["PageID"]))
+            if "PageID" not in doc.keys() or doc["PageID"] == "":
                 pde = PostDataException()
                 pde.message = "Document PageID is Null or empty."
-                print("Error: {}".format(pde))
+                print("Validators:  Error: {}".format(pde))
                 raise
-
     except Exception as e:
-        print("Error:  {}".format(e))
+        print("Validators: Error:  {}".format(e))
         raise e
     return True
+    # try:
+    #     if event is None:
+    #         pde = PostDataException()
+    #         pde.message = "POST requires at least a single document."
+    #         print("Error: {}".format(pde))
+    #         raise
+    #     docs = event
+    #     for doc in docs:
+    #         if doc["PageID"] is None or doc["PageID"] == "":
+    #             pde = PostDataException()
+    #             pde.message = "Document PageID is Null or empty."
+    #             print("Error: {}".format(pde))
+    #             raise
+    #
+    # except Exception as e:
+    #     print("Error:  {}".format(e))
+    #     raise e
+    # return True
 
 
 def validate_date_format(date_parameter):
